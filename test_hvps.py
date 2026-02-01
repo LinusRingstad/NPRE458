908c2113435beb766867adbc3320dcc0b94bfdff
Linus R <linussr2@illinois.edu>
2026-02-01 16:31:20 -0600

    slowed down

--- test_hvps.py
+++ test_hvps.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     })
 
     speeds = list(np.linspace(0, 1, 10)) + list(np.linspace(1, 0, 10))
-    dt = 1
+    dt = 5
     motor1.stop()
     motor2.stop()
     time.sleep(dt)
