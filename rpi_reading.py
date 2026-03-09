7e1bb15cb7921a27f8dd77cb86ed04a9d89de5d8
Linus R <linussr2@illinois.edu>
2026-03-08 19:48:19 -0500

    changed rpi reading

--- rpi_reading.py
+++ rpi_reading.py
@@ -1,6 +1,6 @@
 import serial
 
 ser = serial.Serial('/dev/ttyUSB0', 115200)  # Adjust port as needed
-for i in range(10):
+while True:
     data = ser.readline().decode('utf-8').strip()
     print(data)
